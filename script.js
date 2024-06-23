f58ad94e41e82031f24fd6c79dd27bcf0f675d76
rizkazaqi <rizkazaqi@gmail.com>
2024-06-23 22:19:39 +0700

    update the engagement content

---
+++ script.js
@@ -0,0 +1,11 @@
+function addToCalendar() {
+    const title = "Engagement of Rizka & Fajar";
+    const location = "Bumi Apsari, https://maps.app.goo.gl/YTZC6VE47wrZQUbu8";
+    const details = "We are requesting the honor of your presence at our engagement day.";
+    const startDate = "20240810T153000Z";
+    const endDate = "20240810T173000Z";
+
+    const url = `https://www.google.com/calendar/render?action=TEMPLATE&text=${encodeURIComponent(title)}&dates=${startDate}/${endDate}&details=${encodeURIComponent(details)}&location=${encodeURIComponent(location)}&sf=true&output=xml`;
+
+    window.open(url, "_blank");
+}
